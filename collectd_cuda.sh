762302c9fc19f922c0df0a9f8b79126a1f31b072
Kamil Wilczek <kamil.van.wilczek@gmail.com>
2017-11-04 14:48:34 +0100

    Minor improvements in text.

--- collectd_cuda.sh
+++ collectd_cuda.sh
@@ -19,10 +19,10 @@ else
 	source "$config_file"
 fi
 
-# This parameter will always be present. This is a way to
+# This parameter will always be present. It is a way to
 # uniquely identify a GPU card (very useful in multiGPU
 # machines). nvidia-smi also accepts this ID as a parameter
-# in many queries. Alternatively one can use 'uuid'.
+# in many queries. Alternatively, one can use 'uuid'.
 query_string="pci.bus_id,"
 
 # Building the nvidia-smi --query-gpu string.
@@ -37,16 +37,16 @@ done
 gpus_state=$(nvidia-smi --query-gpu="${query_string%,}" --format=csv,noheader,nounits)
 
 # Output for collectd.
-# Here bash variable indirection is used.
-# For example, variable name 'power_draw" is used as a parameter in
-# collectd protocol and it also has value assigned to that name.
+# Bash variable indirection is used here.
+# For example, variable name 'power_draw' is used as a parameter in
+# collectd protocol and it also has a value assigned to that name.
 while IFS=',' read -r gpu_id "${!config[@]}"
 do
 	for parameter in "${!config[@]}"
 	do
-		# GPU power state, 'pstate', is returned in as a number with
+		# GPU power state, 'pstate', is returned as a number with
 		# 'P' prefix. The easiest way to remove the 'P' without 
-		# additional loops and conditions is to simply expand the
+		# additional loops and conditions, is to simply expand the
 		# 'parameter' variable with '//P'.
 		echo "PUTVAL ${HOSTNAME}/cuda-${gpu_id}/${config[$parameter]}-${parameter} interval=$INTERVAL N:${!parameter//P}"
 	done
